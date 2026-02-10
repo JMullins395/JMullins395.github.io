50d9ccbc37bae77ec8d3d24a0a83c12c9aefe4e4
Jack Mullins <mullinsjc@hendrix.edu>
2026-02-09 20:28:14 -0600

    1st button layout attempt + alignments for bootstrap columns

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,17 +1,26 @@
 async function getInsult() {
     // change to grab a separate "generated insult" that will go inside "insult-generator"
-    const insultGenerator = document.getElementById('insult-generator');
+    const insultGenerated = document.getElementById('insult-generated');
     // make a button
     try {
         const response = await fetch('https://evilinsult.com/generate_insult.php?lang=en&type=json');
 
         if (!response.ok) {
-            throw new Error('Ran into a problem.');
+            throw new Error('Ran into a problem: ${response.status}');
         }
 
         // remember to parse as json here if the built in doesn't work for some reason
         // also refer to specific element names to figure out what to display to which elements.
-        const data = await response;
-        insultGenerator = data.sdfghgfdf
+        const data = await response.json();
+        insultGenerated = data.insult;
+        console.log(data);
+
+    } catch (error) {
+        console.error('Fetch error:', error);
+        insultGenerated = 'Failed to load';
     }
 }
+
+const insultGetButton = document.getElementById('get-insult-button');
+
+insultGetButton.addEventListener('click', getInsult);
