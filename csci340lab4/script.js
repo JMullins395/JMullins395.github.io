d06736609902878f0ad74ce66ced4ea66a784163
Jack Mullins <mullinsjc@hendrix.edu>
2026-03-04 12:21:11 -0600

    API URLs work (finally)

--- csci340lab4/script.js
+++ csci340lab4/script.js
@@ -1,13 +1,16 @@
+var user_loc = ''
+
 $(document).ready(function() {
-    $('#open-meteo-data').click(function() {
-        var lat = $('#lat-input').val();
-        var lon = $('#lon-input').val();
-        var apiURL = 
+    $('#rand-user').click(function() {
         $.ajax({
             dataType: "json",
-            url: "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=sunrise,sunset,weather_code,cloud_cover_mean,temperature_2m_max,temperature_2m_min&hourly=weather_code,cloud_cover,temperature_2m,relative_humidity_2m&current=weather_code,cloud_cover,is_day,temperature_2m&wind_speed_unit=mph&temperature_unit=fahrenheit&precipitation_unit=inch",
+            url: "https://randomuser.me/api/",
             success: function(results) {
                 console.log(results);
+                var user = results.results[0];
+                user_loc = user.location.country;
+                console.log('User country: ', user_loc)
+                // Add post-test logic here
             },
             error: function(xhr,status,error) {
                 console.log(error);
@@ -17,19 +20,20 @@ $(document).ready(function() {
 })
 
 $(document).ready(function() {
-    $('#iss-overhead').click(function() {
-        var lat = $('#lat-input').val();
-        var lon = $('#lon-input').val();
-        /* var height = $('#height-input').val(); */
+    $('#country-stuff').click(function() {
+        if (user_loc == '') {
+            console.log('Please get a user first.');
+            return;
+        }
 
-        var apiURL = 'https://api.open-notify.org/iss-pass.json?lat='+lat+'&lon='+lon;
+        countryUrl = 'https://restcountries.com/v3.1/name/' + encodeURIComponent(user_loc.trim());
 
         $.ajax({
             dataType: "json",
-            url: apiURL,
+            url: countryUrl,
             success: function(results) {
                 console.log(results);
-
+                // add logic if test
             },
             error: function(xhr,status,error) {
                 console.log(error);
